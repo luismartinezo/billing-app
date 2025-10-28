7ad836747db40f6a9b3f7cab67d66d0da5a61e8a
Luis Martinez <lmartinezocoro@gmail.com>
2025-10-28 12:18:48 +0100

    Add Login Roles Users

---
+++ backend/src/main/java/com/springboot/backend/andres/usersapp/usersbackend/entities/User.java
@@ -0,0 +1,73 @@
+package com.springboot.backend.andres.usersapp.usersbackend.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import static jakarta.persistence.GenerationType.*;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.springboot.backend.andres.usersapp.usersbackend.models.IUser;
+
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
+import jakarta.persistence.UniqueConstraint;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User implements IUser {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String lastname;
+
+    @NotEmpty
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(min=4, max = 12)
+    private String username;
+
+    @Getter
+    @Transient
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private boolean admin;
+
+    @NotBlank
+    private String password;
+
+    @JsonIgnoreProperties({"handler", "hibernateLazyInitializer"})
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+        name="users_roles",
+        joinColumns = {@JoinColumn(name="user_id")},
+        inverseJoinColumns = @JoinColumn(name="role_id"),
+        uniqueConstraints = { @UniqueConstraint(columnNames = {"user_id", "role_id"})}
+    )
+    private List<Role> roles;
+
+}
---
+++ backend/src/main/java/com/springboot/backend/andres/usersapp/usersbackend/models/UserRequest.java
@@ -0,0 +1,35 @@
+package com.springboot.backend.andres.usersapp.usersbackend.models;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserRequest implements IUser {
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String lastname;
+
+    @NotEmpty
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(min = 4, max = 12)
+    private String username;
+
+    private boolean admin;
+
+
+    @Override
+    public boolean isAdmin() {
+        return admin;
+    }
+}
